b4632e6508f75350ce772e82e74e967745bbf1e7
AnishFathimaS <anisfathi0808@gmail.com>
2025-08-31 21:59:06 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Home from './Pages/Home'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import About from '../Components/About'
+import Skill from '../Components/Skill'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+import Project from '../Components/Project'
+import Contact from '../Components/Contact'
+import Education from '../Components/Education'
+import Experience from '../Components/Experience'
+import Certificate from '../Components/Certificate'
+
+const Home = () => {
+  return (
+    <>
+        <Header />
+        <Hero />
+        <About />
+        <Skill />
+        <Experience />
+        <Education />
+        <Project />
+        <Certificate />
+        <Contact />
+        <Footer />    
+    </>
+  )
+}
+
+export default Home
