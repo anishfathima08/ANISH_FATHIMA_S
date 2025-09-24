3e6ae1f2840c02c2585a990a423372b645fc1708
AnishFathimaS <anisfathi0808@gmail.com>
2025-09-24 17:39:53 +0530

    commit

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -101,13 +101,13 @@ const Hero = () => {
           data-aos-duration="1200"
         >
           {/* Resume Button */}
-<a
-  href={assets.MyResume}
-  target="_blank"
-  className="flex items-center gap-2 px-6 py-3 border border-white text-white rounded-lg hover:bg-white hover:text-black transition duration-300"
->
-  <FaDownload /> Download Resume
-</a>
+        <a
+          href={assets.MyResume}
+          target="_blank"
+          className="flex items-center gap-2 px-6 py-3 border border-white text-white rounded-lg hover:bg-white hover:text-black transition duration-300"
+        >
+          <FaDownload /> Download Resume
+        </a>
 
 
 
